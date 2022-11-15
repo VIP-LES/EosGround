648d6d79eb2ad194a01a7c3d913e8c2e98f2e4d2
David-Rey <davidreynolds0809@gmail.com>
2022-11-15 13:47:15 -0500

    added script to create table

---
+++ EosGround/database/db_setup.py
@@ -0,0 +1,39 @@
+
+import psycopg2
+from config.config import config
+
+conn_params = config('database.ini')
+connection = psycopg2.connect(**conn_params)
+connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+cur = connection.cursor()
+
+cur.execute(
+    """
+    CREATE TABLE receive_table (
+    id INT PRIMARY KEY,
+    packet_type INT NOT NULL,
+    packet_sender INT NOT NULL,
+    packet_priority INT NOT NULL,
+    packet_generate_time timestamp NOT NULL,
+    packet_sequence_number INT NOT NULL,
+    packet_timestamp timestamp NOT NULL,
+    packet_body VARCHAR(255) NOT NULL,
+    time_arrived timestamp NOT NULL
+    )
+    """
+)
+
+cur.execute(
+    """
+    CREATE TABLE transmit_table (
+    id INT PRIMARY KEY,
+    time_sent timestamp default NULL,
+    packet_type INT NOT NULL,
+    packet_sender INT NOT NULL,
+    packet_priority INT NOT NULL,
+    packet_generate_time timestamp NOT NULL,
+    packet_body VARCHAR(255) NOT NULL
+    )
+    """
+)
+
