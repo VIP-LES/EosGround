9d6c4649274e640a96ede40f522d187819e3110c
panya <pgbhinder@gmail.com>
2022-11-15 13:46:58 -0500

    made raw data table and connected to webserver

--- postgresDB/dbApp/views.py
+++ postgresDB/dbApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from dbApp.models import RawData
+from django.views import View
+
+class DataTable(View):
+    def get_data(self, request):
+        allData = RawData.objects.all()
+        for data in allData:
+            print(data.packet_type)
+            print(data.priority)
+            print(data.device)
+            print(data.body)
+            print()
+
+        return render(request, 'rawdata.html')
