8e03c3d57caa682789c121737bf5c9d897b86862
aryanbattula <aryan.battula1@gmail.com>
2023-04-12 13:39:37 -0400

    ready for review

--- EosGround/sendCommand.py
+++ EosGround/sendCommand.py
@@ -1,13 +1,11 @@
 
-from EosLib.packet.definitions import Priority, Type
-from EosLib.device import Device
+from EosLib.packet.definitions import Device, Priority, Type
 from config.config import get_config
-import os
 
 import psycopg2
 import datetime
 
-conn_params = get_config(os.path.join('config', 'database.ini'))  # gets config params
+conn_params = get_config('database.ini')  # gets config params
 conn = psycopg2.connect(**conn_params)  # gets connection object
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)  # sets up auto commit
 cursor = conn.cursor()  # creates cursor
@@ -18,12 +16,12 @@ def send_command():
     packet_sender = Device.GROUND_STATION_1
     packet_type = Type.DATA
     packet_priority = Priority.DATA
-    packet_destination = Device.MISC_ENGINEERING_2
+    packet_destination = Device.MISC_RADIO_1
     packet_body = "PING 422"
 
     cursor.execute(
         """
-        INSERT INTO eos_schema.transmit_table (packet_type, sender, priority, destination, generate_time, body) VALUES 
+        INSERT INTO transmit_table (packet_type, packet_sender, packet_priority, packet_destination, packet_generate_time, packet_body) VALUES 
         (%s,%s,%s,%s,%s,%s)
         """, (packet_type, packet_sender, packet_priority, packet_destination, packet_generate_time, packet_body)
     )
