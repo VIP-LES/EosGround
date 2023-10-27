fd718d214c55e163f7713153a539cdcab94ad742
Ian Wood <ianwood103@gmail.com>
2023-10-27 13:28:32 -0400

    add cors policy to django api

--- postgresDB/postgresDB/settings.py
+++ postgresDB/postgresDB/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'dbApp',
     'rest_framework',
+		"corsheaders",
 ]
 
 MIDDLEWARE = [
+		"corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -86,7 +88,7 @@ DATABASES = {
         },
         'NAME': 'eos_db',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -127,3 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
