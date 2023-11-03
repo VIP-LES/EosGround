c56b41e1dc6db50d7edbbafbc51d9e340f915b27
Ion Li <fuzhou.ion@gmail.com>
2023-11-03 13:28:37 -0400

    modified to use a form

--- ground-station-openmct/terminal/terminal_logic.js
+++ ground-station-openmct/terminal/terminal_logic.js
@@ -1,22 +1,21 @@
 const terminal = document.getElementById("terminal");
 const input = document.getElementById("input");
+const form = document.getElementById("terminalForm");
 
-input.addEventListener("keydown", function (event) {
-if (event.key === "Enter") {
-  event.preventDefault();
-  const command = input.value;
-  input.value = "";
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const command = input.value;
+    input.value = "";
 
-  terminal.innerHTML += `$ ${command}\n`;
+    terminal.innerHTML += `$ ${command}\n`;
 
-  if (command === "clear") {
-    terminal.innerHTML = "";
-  } else if (command === "cutdown") {
-    terminal.innerHTML += "success\n";
-  } else {
-    terminal.innerHTML += `Command not found: ${command}\n`;
-  }
+    if (command === "clear") {
+      terminal.innerHTML = "";
+    } else if (command === "cutdown") {
+      terminal.innerHTML += "success\n";
+    } else {
+      terminal.innerHTML += `Command not found: ${command}\n`;
+    }
 
-  terminal.scrollTop = terminal.scrollHeight - terminal.clientHeight;
-}
+    terminal.scrollTop = terminal.scrollHeight - terminal.clientHeight;
 });
