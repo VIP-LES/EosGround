38163a0fc723ece65b1972b44f9ba0d78c0044af
aryanbattula <aryan.battula1@gmail.com>
2023-11-09 20:43:33 -0500

    efield pipeline made

--- EosGround/database/models/eos/e_field.py
+++ EosGround/database/models/eos/e_field.py
@@ -1,20 +1,20 @@
-from sqlalchemy import Identity, Integer, TIMESTAMP, ForeignKey, DOUBLE_PRECISION
+from sqlalchemy import Identity, Integer, ForeignKey, DOUBLE_PRECISION
 from sqlalchemy.orm import Mapped, mapped_column
-from datetime import datetime
 
 from EosGround.database.models import TableBase
 from EosGround.database.models.eos import SCHEMA
 
-
 # Creates a model of the Position table and maps python types to their PostgreSQL counterparts
 #describes what the table looks like in code
-class Position(TableBase):
-    __tablename__ = 'position'
+
+
+class EField(TableBase):
+    __tablename__ = 'e_field'
     __table_args__ = {'schema': SCHEMA}
 
     id: Mapped[int] = mapped_column(Integer, Identity(start=1), primary_key=True, init=False)
     packet_id: Mapped[int] = mapped_column(ForeignKey("eos_schema.received_packets.id"))
-    timestamp: Mapped[datetime] = mapped_column(TIMESTAMP)
     a_voltage: Mapped[float] = mapped_column(DOUBLE_PRECISION)
     b_voltage: Mapped[float] = mapped_column(DOUBLE_PRECISION)
     c_voltage: Mapped[float] = mapped_column(DOUBLE_PRECISION)
+
