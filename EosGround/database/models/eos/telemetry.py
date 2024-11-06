57834beb571fc7596e8570220adc7e56f67d7acb
Chris Krautheim <ckrautheim3@gatech.edu>
2024-11-06 13:04:58 -0500

    added datetime to telemtry.py

--- EosGround/database/models/eos/telemetry.py
+++ EosGround/database/models/eos/telemetry.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Identity, Integer, ForeignKey, DOUBLE_PRECISION
+from sqlalchemy import Identity, Integer, ForeignKey, DOUBLE_PRECISION, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from EosGround.database.models import TableBase
 from EosGround.database.models.eos import SCHEMA
 
+import datetime
 
 class Telemetry(TableBase):
     __tablename__ = 'telemetry'
@@ -11,6 +12,7 @@ class Telemetry(TableBase):
 
     id: Mapped[int] = mapped_column(Integer, Identity(start=1), primary_key=True, init=False)
     packet_id: Mapped[int] = mapped_column(ForeignKey("eos_schema.received_packets.id"))
+    timestamp: Mapped[datetime] = mapped_column(DateTime)
     temperature: Mapped[float] = mapped_column(DOUBLE_PRECISION)
     pressure: Mapped[float] = mapped_column(DOUBLE_PRECISION)
     humidity: Mapped[float] = mapped_column(DOUBLE_PRECISION)
